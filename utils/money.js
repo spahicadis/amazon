6e2c705a74f7bccc6748645b9f03c5a14a7b7d05
spahicadis <adis@spahica.com>
2024-09-02 10:29:22 +0200

    Save data and generate HTML for delivery options

--- utils/money.js
+++ utils/money.js
@@ -2,3 +2,6 @@ export function formatCurrency(priceCents) {
 return (priceCents / 100).toFixed(2)
 
 } 
+
+
+export default formatCurrency; //jednostavniji nacin exporta sada mi je dosao aha momenat jer sam se susreo sa ovime u projektu dvd React. Pa onda sada kada importamo ne moramo koristiti { } i svaki fajl moze imati samo jedan default export
