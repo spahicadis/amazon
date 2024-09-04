164569a4d6a0f6df1e7822567d21700831a01d94
spahicadis <adis@spahica.com>
2024-09-04 13:00:56 +0200

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,6 @@
+import { formatCurrency } from '../utils/money.js'
+
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -9,6 +12,45 @@ export function getProduct(productId) {
   
     return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+getStarsUrl() {
+ return `images/ratings/rating-${this.rating.stars * 10}.png`
+}
+
+
+getPrice() {
+  return `${formatCurrency(this.priceCents)}`
+
+}
+
+}
+
+const product1 = new Product( {
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+})
+console.log(product1);
 
 export const products = [
   {
@@ -669,4 +711,10 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+return new Product(productDetails);
+});
+
+console.log(products)
+
+
