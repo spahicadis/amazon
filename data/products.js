49142884a2b49a8d1ded984fcdb6d07ed35a2de1
spahicadis <adis@spahica.com>
2024-09-05 10:45:15 +0200

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -111,6 +111,33 @@ const product1 = new Product( {
 })
 console.log(product1);
 
+
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails)
+      }
+      return new Product(productDetails);
+      });;
+
+      console.log('load products')
+      fun();
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -778,5 +805,5 @@ return new Product(productDetails);
 });
 
 console.log(products)
-
+*/;
 
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,96 +1,99 @@
 import { cart, addToCart } from "../data/cart.js" //moze i npr as customerCart, pa koristimo onda naziv cart za nesto drugo
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 
 
+loadProducts(renderProductsGrid);
 
 
-let productsHTML = '';
-products.forEach((product) => {
-  productsHTML += `  <div class="product-container"> 
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+function renderProductsGrid() {
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="${product.getStarsUrl()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  let productsHTML = '';
+  products.forEach((product) => {
+    productsHTML += `  <div class="product-container"> 
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
-          </div>
 
-          <div class="product-price">
-            "${product.getPrice()}" 
-          </div>
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="${product.getStarsUrl()}">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              "${product.getPrice()}" 
+            </div>
+
+            <div class="product-quantity-container">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
 
 
-          ${product.extraInfoHTML()}
+            ${product.extraInfoHTML()}
 
 
-          <div class="product-spacer"></div>
+            <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-id="${product.id}"> 
-            Add to Cart
-          </button>
-        </div>`
-  ;
-});
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}"> 
+              Add to Cart
+            </button>
+          </div>`
+    ;
+  });
 
-console.log(productsHTML);
+  console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+  document.querySelector('.js-products-grid').innerHTML = productsHTML
 
-function updateCartQuantity() {
-  let totalQuantity = 0;
-  cart.forEach((item) => {
-  totalQuantity+=item.quantity;
-})
+  function updateCartQuantity() {
+    let totalQuantity = 0;
+    cart.forEach((item) => {
+    totalQuantity+=item.quantity;
+  })
 
-document.querySelector('.js-cart-quantity').innerHTML = totalQuantity
+  document.querySelector('.js-cart-quantity').innerHTML = totalQuantity
 
-console.log(totalQuantity)
-  
+  console.log(totalQuantity)
+    
+  }
+
+  const buttons = document.querySelectorAll('.js-add-to-cart')
+  buttons.forEach((button, index) => {
+    button.addEventListener('click',() => {
+      console.log(`Added product ${index}`)
+      /*cart.push({
+        image: products[index].image
+      })*/ //Ovako bi ja napravio, ali u projektu koristimo dataset za dohvacanje pojedinog elementa pa cu primjer i ideju pustiti samo zakomentiranu
+    const productId = button.dataset.productId; //kebabCase to camelCase convert
+    addToCart(productId);
+    updateCartQuantity();
+
+    console.log(cart)
+    }) 
+  })
 }
-
-const buttons = document.querySelectorAll('.js-add-to-cart')
-buttons.forEach((button, index) => {
-  button.addEventListener('click',() => {
-    console.log(`Added product ${index}`)
-    /*cart.push({
-      image: products[index].image
-    })*/ //Ovako bi ja napravio, ali u projektu koristimo dataset za dohvacanje pojedinog elementa pa cu primjer i ideju pustiti samo zakomentiranu
-  const productId = button.dataset.productId; //kebabCase to camelCase convert
-  addToCart(productId);
-  updateCartQuantity();
-
-  console.log(cart)
-  }) 
-})
-
